fdd39b3795638317f22bfd7a8cd421af1a85320d
Mikhail Nikanorov <31664472+Caribace@users.noreply.github.com>
2019-06-26 12:33:16 +0300

    Update guestbook.py

--- 12/guestbook.py
+++ 12/guestbook.py
@@ -46,10 +46,10 @@ class GuestBook:
 
 if __name__ == "__main__":
     guest_book = GuestBook()
-    guest_book.add_guest("Venom", "Dreadful", "venom@space.com")
-    guest_book.add_guest("Harold", "Great", "Harold@gmail.com")
-    guest_book.remove_guest("Harold@gmail.com")
-    guest_book.full_remove_guest("Ribo@gmail.com")
-    guest_book.rename_guest("Harold@gmail.com","REY12","Scalator")
+    guest_book.add_guest("Mikhail", "Nikanorov", "caribace@mail.ru")
+    guest_book.add_guest("Caribace", "Caribovich", "caribovich@mail.ru")
+    guest_book.remove_guest("caribace@mail.ru")
+    guest_book.full_remove_guest("caribovich@mail.ru")
+    guest_book.rename_guest("caribovich@mail.ru","Caribace","Caribovich")
     guest_book.write_file()
     guest_book.file_cleanup()
