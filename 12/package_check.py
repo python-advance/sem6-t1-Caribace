5a97a4007e5cac99940afcb8b691238b5eb46ada
Mikhail Nikanorov <31664472+Caribace@users.noreply.github.com>
2019-06-26 12:35:09 +0300

    Update package_check.py

--- 12/package_check.py
+++ 12/package_check.py
@@ -2,10 +2,10 @@ from guestbookpackage import GuestBook
 
 
 guest_book = GuestBook()
-guest_book.add_guest("Venom", "Dreadful", "venom@space.com")
-guest_book.add_guest("Harold", "Great", "Harold@gmail.com")
-guest_book.remove_guest("Harold@gmail.com")
-guest_book.full_remove_guest("Ribo@gmail.com")
-guest_book.rename_guest("Harold@gmail.com","REY12","Scalator")
+guest_book.add_guest("Mikhail", "Nikanorov", "caribace@mail.ru")
+guest_book.add_guest("Caribace", "Caribovich", "caribovich@mail.ru")
+guest_book.remove_guest("caribace@mail.ru")
+guest_book.full_remove_guest("caribovich@mail.ru")
+guest_book.rename_guest("caribovich@mail.ru","Caribace","Caribovich")
 guest_book.write_file()
 guest_book.file_cleanup()
